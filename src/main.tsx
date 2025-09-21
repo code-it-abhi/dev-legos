3ccae540660f0a1bb018daa51d49b880c983b489
code-it-abhi <abhilashkumar.ofc@gmail.com>
2025-09-21 12:42:24 +0530

    feat: added bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "@/App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/css/index.css";
 
 createRoot(document.getElementById("root")!).render(
